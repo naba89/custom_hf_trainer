1f87574db0ef8a14f6cf23d6fc2cc602f14ec124
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2024-11-30 20:56:33 +0900

    Update custom_trainer_v2.py

--- custom_hf_trainer/custom_trainer_v2.py
+++ custom_hf_trainer/custom_trainer_v2.py
@@ -1,16 +1,23 @@
 """Copyright: Nabarun Goswami (2024)."""
-from typing import Dict, List
+import math
+import time
+from typing import Dict, List, Optional, Union
 
+import datasets
 import torch
+from torch.utils.data import DataLoader, Dataset
 from transformers import Trainer, TrainerCallback, TrainingArguments, TrainerState, TrainerControl, \
-    is_torch_xla_available
+    is_torch_xla_available, is_datasets_available
+from transformers.debug_utils import DebugOption
 from transformers.modeling_utils import unwrap_model
+from transformers.trainer_utils import speed_metrics
 from transformers.utils import logging
 
 logger = logging.get_logger(__name__)
 
 if is_torch_xla_available():
     import torch_xla.core.xla_model as xm
+    import torch_xla.debug.metrics as met
 
 
 class AddExtraLossesToTrainerState(TrainerCallback):
@@ -90,3 +97,165 @@ class CustomTrainer(Trainer):
         if self.control.should_save:
             self._save_checkpoint(model, trial, metrics=metrics)
             self.control = self.callback_handler.on_save(self.args, self.state, self.control)
+
+    def _create_eval_dataloader(self, eval_dataset):
+        logger.warning("Eval dataloader is being created (again)."
+                       "Ignore if this is the first time you are seeing this message."
+                       "Otherwise, check the CustomTrainer implementation.")
+        data_collator = self.data_collator
+
+        if is_datasets_available() and isinstance(eval_dataset, datasets.Dataset):
+            eval_dataset = self._remove_unused_columns(eval_dataset, description="evaluation")
+        else:
+            data_collator = self._get_collator_with_removed_columns(data_collator, description="evaluation")
+
+        dataloader_params = {
+            "batch_size": self.args.eval_batch_size,
+            "collate_fn": data_collator,
+            "num_workers": self.args.dataloader_num_workers,
+            "pin_memory": self.args.dataloader_pin_memory,
+            "persistent_workers": self.args.dataloader_persistent_workers,
+        }
+
+        if not isinstance(eval_dataset, torch.utils.data.IterableDataset):
+            dataloader_params["sampler"] = self._get_eval_sampler(eval_dataset)
+            dataloader_params["drop_last"] = self.args.dataloader_drop_last
+            dataloader_params["prefetch_factor"] = self.args.dataloader_prefetch_factor
+
+        return self.accelerator.prepare(DataLoader(eval_dataset, **dataloader_params))
+
+    def get_eval_dataloader(self,
+                            eval_dataset: Optional[Dataset] = None,
+                            eval_dataset_name: Optional[str] = None) -> DataLoader:
+        """
+        Returns the evaluation [`~torch.utils.data.DataLoader`].
+
+        Subclass and override this method if you want to inject some custom behavior.
+
+        Args:
+            eval_dataset (`torch.utils.data.Dataset`, *optional*):
+                WARNING: Not used here, only for signature compatibility.
+                If provided, will override `self.eval_dataset`. If it is a [`~datasets.Dataset`], columns not accepted
+                by the `model.forward()` method are automatically removed. It must implement `__len__`.
+            eval_dataset_name (`str`, *optional*):
+
+        """
+        if eval_dataset is not None:
+            raise ValueError("In this custom trainer, you need to pass the name of dataset")
+
+        if eval_dataset_name is None and self.eval_dataset is None:
+            raise ValueError("Trainer: evaluation requires an eval_dataset.")
+        if self.eval_dataloader is None:
+            # create all eval dataloaders if they don't exist
+            if isinstance(self.eval_dataset, dict):
+                self.eval_dataloader = {}
+                for _eval_dataset_name, _eval_dataset in self.eval_dataset.items():
+                    self.eval_dataloader[_eval_dataset_name] = self._create_eval_dataloader(_eval_dataset)
+            else:
+                self.eval_dataloader = self._create_eval_dataloader(self.eval_dataset)
+
+        if isinstance(self.eval_dataloader, dict):
+            return self.eval_dataloader[eval_dataset_name]
+        else:
+            return self.eval_dataloader
+
+    def evaluate(
+        self,
+        eval_dataset: Optional[Union[Dataset, str, Dict[str, Dataset]]] = None,
+        ignore_keys: Optional[List[str]] = None,
+        metric_key_prefix: str = "eval",
+    ) -> Dict[str, float]:
+        """
+        Run evaluation and returns metrics.
+
+        The calling script will be responsible for providing a method to compute metrics, as they are task-dependent
+        (pass it to the init `compute_metrics` argument).
+
+        You can also subclass and override this method to inject custom behavior.
+
+        Args:
+            eval_dataset (Union[`str`, Dict[str, `Dataset`]), *optional*):
+                Pass a dataset if you wish to override `self.eval_dataset`. If it is a [`~datasets.Dataset`], columns
+                not accepted by the `model.forward()` method are automatically removed. If it is a dictionary, it will
+                evaluate on each dataset, prepending the dictionary key to the metric name. Datasets must implement the
+                `__len__` method.
+
+                <Tip>
+
+                If you pass a dictionary with names of datasets as keys and datasets as values, evaluate will run
+                separate evaluations on each dataset. This can be useful to monitor how training affects other
+                datasets or simply to get a more fine-grained evaluation.
+                When used with `load_best_model_at_end`, make sure `metric_for_best_model` references exactly one
+                of the datasets. If you, for example, pass in `{"data1": data1, "data2": data2}` for two datasets
+                `data1` and `data2`, you could specify `metric_for_best_model="eval_data1_loss"` for using the
+                loss on `data1` and `metric_for_best_model="eval_data1_loss"` for the loss on `data2`.
+
+                </Tip>
+
+            ignore_keys (`List[str]`, *optional*):
+                A list of keys in the output of your model (if it is a dictionary) that should be ignored when
+                gathering predictions.
+            metric_key_prefix (`str`, *optional*, defaults to `"eval"`):
+                An optional prefix to be used as the metrics key prefix. For example the metrics "bleu" will be named
+                "eval_bleu" if the prefix is "eval" (default)
+
+        Returns:
+            A dictionary containing the evaluation loss and the potential metrics computed from the predictions. The
+            dictionary also contains the epoch number which comes from the training state.
+        """
+        # handle multipe eval datasets
+        override = eval_dataset is not None
+        eval_dataset = eval_dataset if override else self.eval_dataset
+        if isinstance(eval_dataset, dict):
+            metrics = {}
+            for eval_dataset_name in eval_dataset.keys():
+                dataset_metrics = self.evaluate(
+                    eval_dataset=eval_dataset_name,
+                    ignore_keys=ignore_keys,
+                    metric_key_prefix=f"{metric_key_prefix}_{eval_dataset_name}",
+                )
+                metrics.update(dataset_metrics)
+            return metrics
+
+        # memory metrics - must set up as early as possible
+        self._memory_tracker.start()
+
+        eval_dataloader = self.get_eval_dataloader(eval_dataset_name=eval_dataset)
+        start_time = time.time()
+
+        eval_loop = self.prediction_loop if self.args.use_legacy_prediction_loop else self.evaluation_loop
+        output = eval_loop(
+            eval_dataloader,
+            description="Evaluation",
+            # No point gathering the predictions if there are no metrics, otherwise we defer to
+            # self.args.prediction_loss_only
+            prediction_loss_only=True if self.compute_metrics is None else None,
+            ignore_keys=ignore_keys,
+            metric_key_prefix=metric_key_prefix,
+        )
+
+        total_batch_size = self.args.eval_batch_size * self.args.world_size
+        if f"{metric_key_prefix}_jit_compilation_time" in output.metrics:
+            start_time += output.metrics[f"{metric_key_prefix}_jit_compilation_time"]
+        if f"{metric_key_prefix}_model_preparation_time" in output.metrics:
+            start_time += output.metrics[f"{metric_key_prefix}_model_preparation_time"]
+        output.metrics.update(
+            speed_metrics(
+                metric_key_prefix,
+                start_time,
+                num_samples=output.num_samples,
+                num_steps=math.ceil(output.num_samples / total_batch_size),
+            )
+        )
+
+        self.log(output.metrics)
+
+        if DebugOption.TPU_METRICS_DEBUG in self.args.debug:
+            # tpu-comment: Logging debug metrics for PyTorch/XLA (compile, execute times, ops, etc.)
+            xm.master_print(met.metrics_report())
+
+        self.control = self.callback_handler.on_evaluate(self.args, self.state, self.control, output.metrics)
+
+        self._memory_tracker.stop_and_update_metrics(output.metrics)
+
+        return output.metrics
